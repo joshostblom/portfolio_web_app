5c49b0af924850312c453a7b8c288e9a456ad8e1
joshostblom <josh.ostblom@gmail.com>
2024-06-09 10:09:12 -0500

    Remove react-bootstrap

--- src/components/LocalNavbar.jsx
+++ src/components/LocalNavbar.jsx
@@ -1,19 +1,15 @@
-import Container from 'react-bootstrap/Container';
-import Nav from 'react-bootstrap/Nav';
-import Navbar from 'react-bootstrap/Navbar';
-
 function LocalNavbar() {
     return (
-        <Navbar>
-            <Container className="flex bg-slate-200 dark:bg-slate-800 p-3 rounded-lg">
-                <Navbar.Brand className="font-bold" href="/">Joshua Ostblom</Navbar.Brand>
-                <Nav>
-                    <Nav.Link className="px-2" href="/">Home</Nav.Link>
-                    <Nav.Link className="px-2" href="/resume">Resume</Nav.Link>
-                    <Nav.Link className="px-2" href="/projects">Projects</Nav.Link>
-                </Nav>
-            </Container>
-        </Navbar>
+        <div>
+            <div className="flex bg-slate-200 dark:bg-slate-800 p-3 rounded-lg">
+                <h1 className="font-bold" href="/">Joshua Ostblom</h1>
+                <nav>
+                    <a className="px-2" href="/">Home</a>
+                    <a className="px-2" href="/resume">Resume</a>
+                    <a className="px-2" href="/projects">Projects</a>
+                </nav>
+            </div>
+        </div>
     );
 }
 
