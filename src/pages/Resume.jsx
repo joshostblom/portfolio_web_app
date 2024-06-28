22e29892ddabfbc9285653bc203ec8593e318657
joshostblom <josh.ostblom@gmail.com>
2024-06-27 20:17:57 -0500

    Fix resume display.

--- src/pages/Resume.jsx
+++ src/pages/Resume.jsx
@@ -1,5 +1,22 @@
+import { Document, Page, pdfjs } from "react-pdf";
+
+pdfjs.GlobalWorkerOptions.workerSrc = new URL(
+  "pdfjs-dist/build/pdf.worker.min.mjs",
+  import.meta.url
+).toString();
+
 function Resume() {
-  return <div>Resume</div>;
+  return (
+    <div className="flex w-auto justify-center">
+      <Document file="http://localhost:8080/resources/get/resume">
+        <Page
+          pageNumber={1}
+          renderAnnotationLayer={false}
+          renderTextLayer={false}
+        />
+      </Document>
+    </div>
+  );
 }
 
 export default Resume;
